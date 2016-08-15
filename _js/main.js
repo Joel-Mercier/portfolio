051ac21f0a0b964e75bb15909da3e9d942fa8882
Joel Mercier <joelmercier@pc50.home>
2016-08-15 23:08:13 +0200

    portfolio

--- _js/main.js
+++ _js/main.js
@@ -1,6 +1,47 @@
-$('.type-it').typeIt({
-  strings: ['Joel Mercier', 'développeur web'],
-  speed: 100,
-  lifeLike: false,
-  cursor: true  
+$( document ).ready(function() {
+
+  var typeTitle = function(element, strings) {
+    // typeIt jQuery plugin init
+    $(element).typeIt({
+      strings: strings,
+      speed: 100,
+      lifeLike: false,
+      cursor: true
+    });
+  };
+
+  typeTitle('.header__title.type-it', ['Joel Mercier', 'Développeur web']);
+
+  $('#barba__wrapper').on('mouseover', '.portfolio__link', function(event) {
+    var currentImage = $('.portfolio__image--current');
+    var portfolioImg = $(this).parent().data('img');
+    var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+    currentImage.css('background-image', 'url('+ portfolioImg +')');
+    currentImage.addClass('animated fade-in').one(animationEnd, function() {
+      currentImage.removeClass('animated fade-in');
+    });
+  });
+
+  $('#barba__wrapper').on('mouseleave', '.portfolio__link', function(event) {
+    var currentImage = $('.portfolio__image--current');
+    var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+    currentImage.addClass('animated fade-out').one(animationEnd, function() {
+      currentImage.css('background-image', '');
+      currentImage.removeClass('animated fade-out');
+    });
+  });
+
+  // BarbaJS plugin init
+  Barba.Pjax.Dom.wrapperId = 'barba__wrapper';
+  Barba.Pjax.Dom.containerClass = 'barba__container';
+  Barba.Pjax.ignoreClassLink = 'barba__link--ignored';
+
+  Barba.Prefetch.init();
+  Barba.Pjax.start();
+
+  Barba.Dispatcher.on('newPageReady', function(currentStatus, oldStatus, container) {
+    typeTitle('.header__title.type-it', ['Joel Mercier', 'Développeur web']);
+
+  });
+
 });
